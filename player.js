3537bed0307e08e33005606939828b4324b24f7f
JoshMcDonough <51344351+JoshMcDonough@users.noreply.github.com>
2021-11-05 14:21:37 +1300

    Add files via upload

--- player.js
+++ player.js
@@ -1,19 +1,37 @@
-class Player{ 
-    constructor(x,y,w,h,c,xspeed,yspeed){
-        this.x= x;
-        this.y =y;
-        this.w= w;
-        this.h =h;
-        this.c= c;
-        this.xspeed =xspeed;
-        this.yspeed= yspeed;
+class Player {
+    constructor(x, y, w, h, c, xspeed, yspeed) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
     }
 
-        
 
-    playerDraw(){ 
+
+    playerDraw() {
         canvasContext.fillStyle = this.c;
-        canvasContext.fillRect(this.x,this.y,this.w,this.h);
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
 
     }
-}
+
+    playerMove (){ // Player movement through the press of space bar
+       this.x = canvas.width / 6;
+
+        if(spaceKeyPressed){ 
+           this.yspeed = -9;
+//            console.log("jumping");
+           }
+        this.yspeed += 0.5;
+        this.y += this.yspeed;
+        this.yspeed *= 0.98;
+//        if(this.y < 0 || this.y + this.h > canvas.height) {
+//            gameRun = false;
+//            console.log("Hit top");
+//        }
+    }
+    }
+
+
