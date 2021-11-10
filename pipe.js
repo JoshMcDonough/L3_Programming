c8cebfe02ed275e0791e82067b51d3a5b25ebb0a
JoshMcDonough <51344351+JoshMcDonough@users.noreply.github.com>
2021-11-11 02:41:44 +1300

    Add files via upload

--- pipe.js
+++ pipe.js
@@ -1,4 +1,4 @@
-class Pipe { 
+class Pipe { //Main class for the pipes
     constructor(x,y,w,h,c,xspeed){
         this.x= x;
         this.y =y;
@@ -7,17 +7,17 @@ class Pipe {
         this.c= c;
         this.xspeed =xspeed;
     }
-     pipeDraw(){ 
+     pipeDraw(){ //Function that draws the pipes on the canvas 
         canvasContext.fillStyle = this.c;
         canvasContext.fillRect(this.x,this.y,this.w,this.h);
-    }
-     pipeMove(){ 
+    }//end of func
+     pipeMove(){ //Makes the pipes move from the right side of the screen to the left 
         this.x -= this.xspeed;
-    }
+    }//end of func
     score() {
            if (this.x < 0) {
             Score ++;
           }
-        }
+        }//end of func
     
 }
--- player.js
+++ player.js
@@ -11,16 +11,16 @@ class Player {
 
 
 
-    playerDraw() {
+    playerDraw() { //playerDraw Function draws player
         canvasContext.fillStyle = this.c;
         canvasContext.fillRect(this.x, this.y, this.w, this.h);
 
-    }
+    }//end of func
 
-    playerMove() {
+    playerMove() { //Player movement Function through the press of space bar
         this.x = canvas.width / 6;
 
-        if (spaceKeyPressed) {
+        if (spaceKeyPressed) { //If spacekey is pressed increase y speed by -9 , helps gives the feeling of falling and gravity to the player  
             this.yspeed = -9;
         }
         if (this.y < 0 || this.y + this.h > canvas.height) {
@@ -33,34 +33,34 @@ class Player {
         this.y += this.yspeed;
         this.yspeed *= 0.98;
 
-    }
+    }//end of func
     pipehit() {
         if (pxPos + PLAYER_WIDTH == xPos) {
             gameover = true;
         }
-    }
+    }//end of func
 
     pipeHit(item) {
         return (this.x <= (item.x + item.w) &&
                 (this.x + this.w) >= item.x) &&
             (this.y <= (item.y + item.h) &&
                 (this.y + this.h) >= item.y);
-    }
+    }//end of func
     hasPipeHit(pipe) {
         return this.pipeHit(pipe);
-    }
-    hasCollided() {
+    }//end of func
+    hasCollided() { //Collison function
         var self = this;
         var collided = false;
 
         pipes.forEach(function (pipe, i) {
             if (self.hasPipeHit(pipe)) {
 
-                if (self.y + self.h > pipe.y && self.y < pipe.y + pipe.h && self.x + self.w > pipe.x && self.w < pipe.x + pipe.w) {
+                if (self.y + self.h > pipe.y && self.y < pipe.y + pipe.h && self.x + self.w > pipe.x && self.w < pipe.x + pipe.w) { // If Players x or y pos overlaps with pipes x or y pos register collsion and set gameRun to false
                     gameRun = false;
                 }
                 collided = true;
             }
         });
-    }
+    }//end of func
 }
